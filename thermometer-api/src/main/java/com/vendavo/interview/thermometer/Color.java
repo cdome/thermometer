73c3c7bbb10356e49b2b474b375b434d54b99d6b
cdome <1021354+cdome@users.noreply.github.com>
2021-01-21 20:10:03 -0700

    upgraded gradle, upgraded java to 11, upgraded junit, simplified api enum

--- thermometer-api/src/main/java/com/vendavo/interview/thermometer/Color.java
+++ thermometer-api/src/main/java/com/vendavo/interview/thermometer/Color.java
@@ -1,6 +1,7 @@
 package com.vendavo.interview.thermometer;
 
 public enum Color {
-    DARK_BLUE, BLUE, LIGHT_BLUE, GREEN, YELLOW, ORANGE, RED, UNKNOWN
+    //DARK_BLUE, BLUE, LIGHT_BLUE, GREEN, YELLOW, ORANGE, RED, UNKNOWN
+    GREEN, YELLOW, RED
 }
 
